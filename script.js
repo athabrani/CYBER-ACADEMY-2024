80cfe00c5727d2901fbc5c8c754d9bc9497774f5
Athalah Rafif Irsyach Sarbrani <151816923+athabrani@users.noreply.github.com>
2024-11-20 03:10:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function tombol(){
+    alert("SELAMAT DATANG DI CYBER ACADEMY")
+}
+
+const apiKey = '____________';                                                                // Ganti dengan API Key Anda
+const url = `______________`;                                                                //masukkan URL api 
+
+async function fetchNews() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.status === 'ok') {
+            displayNews(data._______);
+        } else {
+            console.error('Error fetching news:', data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function displayNews(articles) {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = ''; 
+
+    limitedArticles = articles.slice(0, __);
+
+    limitedArticles.forEach((article) => {
+        const newsItem = document.createElement('div');
+        newsItem.className = 'news-item';
+
+        newsItem.innerHTML = `
+            <h2>${article.________}</h2>                                                                //sesuaikan bagian rumpang dengan isi dari API
+            <p><strong>________:</strong> ${article._______ }</p>
+            <p><strong>________:</strong> ${new Date(article._________).toLocaleString()}</p>
+            <p>${article.________}</p>
+        `;
+
+        newsContainer.appendChild(newsItem);
+    });
+}
+
+// Panggil fungsi untuk memuat berita saat halaman dimuat
+fetchNews();
